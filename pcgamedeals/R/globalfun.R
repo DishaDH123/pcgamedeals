590cb4a443b16cda94cb3088ee82b7c866695eaa
Neetz78 <neethu.g0807@gmail.com>
2022-02-16 18:21:02 -0800

    Adding the package

---
+++ pcgamedeals/R/globalfun.R
@@ -0,0 +1,1 @@
+utils::globalVariables(c('g','isActive','storeName','storeID','title','normalPrice','salePrice','savings','dealRating','metacriticScore','price_diff','head','reorder'))
---
+++ pcgamedeals/tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(pcgamedeals)
+
+test_check("pcgamedeals")
